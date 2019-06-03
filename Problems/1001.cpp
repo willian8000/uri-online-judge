3e0103bcde4705961321c06cc553e5a7d55c3846
willian8000 <seila123@>
2019-06-03 16:43:36 -0300

    1001

---
+++ Problems/1001.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+ 
+using namespace std;
+
+int main() {
+ 
+    int a = 0;
+    int b = 0;
+    
+    scanf("%d", &a);
+    scanf("%d", &b);
+    
+    printf("X = %d\n", a+b);
+ 
+    return 0;
+}
---
+++ Problems/1001.py
@@ -0,0 +1,6 @@
+# print("X = " + str(int(input()) + int(input())))  # solution with one line
+
+a = int(input())
+b = int(input())
+
+print("X = " + str(a + b))
