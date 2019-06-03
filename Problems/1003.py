560e3d2f585a27338cc8799a1c3c60790b0cb1c3
willian8000 <seila123@>
2019-06-03 18:02:49 -0300

    1005

---
+++ Problems/1003.py
@@ -0,0 +1,2 @@
+a, b = int(input()), int(input())
+print("SOMA = " + str(a + b))
---
+++ Problems/1005.py
@@ -0,0 +1,2 @@
+a, b = float(input()) * (35/110), float(input()) * (75/110)  # 75/100 = 7.5, 110/100 = 1,1
+print("MEDIA = {0:.5f}".format(a + b))
