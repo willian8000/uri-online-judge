8a4e45afd7f0b9bd04ef60bc2a648507ad5a4dc6
willian8000 <seila123@>
2019-06-04 09:11:47 -0300

    1008

---
+++ Problems/1008.py
@@ -0,0 +1,2 @@
+funcionario, horas_trab, valor_hora = int(input()), int(input()), float(input())
+print("NUMBER = " + str(funcionario) + "\n" + "SALARY = U$ {0:.2f}".format(horas_trab * valor_hora))
